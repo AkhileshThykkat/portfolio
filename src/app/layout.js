d2bb0ed53e1d92a554a6547b817cc43c2be05ef7
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-08-09 01:01:32 +0530

    meta data is added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,21 @@
-'use client';
+// 'use client';
+
 import './globals.css';
 import { inter } from './fonts';
 import { michroma } from './fonts';
 import { Providers } from './providers';
+export const metaData = {
+  title: 'Akhilesh Thykkat Portfolio',
+  description:
+    'Welcome to my portfolio website showcasing my work and projects.',
+  author: 'Akhilesh Thykkat',
+  image: '../src/app/assets/me.jpg', // Path to a default thumbnail image
+  url: 'https://thykkat-portfolio.vercel.app/',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/akhilesh-m-t-a68b09220/',
+    github: 'https://github.com/AkhileshThykkat/',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
